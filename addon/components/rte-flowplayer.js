2974345a338d9dfff06c54300b7429beeb2cf216
Doug Banville <doug.banville@rte.ie>
2019-03-27 17:18:47 +0000

    added media session data for android users

--- addon/components/rte-flowplayer.js
+++ addon/components/rte-flowplayer.js
@@ -10,6 +10,54 @@ export default Component.extend({
 
   myFlowplayer: service("rteFlowplayer"),
 
+  mediaSession() {
+    if ("mediaSession" in navigator) {
+      let mediaSessionThumb = "https://img.rasset.ie/0011cb2c";
+      if (this.audioMeta.thumbnailref) {
+        mediaSessionThumb =
+          "https://img.rasset.ie/" + this.audioMeta.thumbnailref;
+      }
+
+      navigator.mediaSession.metadata = new MediaMetadata({
+        title: this.audioMeta.title,
+        artist: this.audioMeta.channel,
+
+        artwork: [
+          {
+            src: mediaSessionThumb + "-96.jpg",
+            sizes: "96x96",
+            type: "image/jpeg"
+          },
+          {
+            src: mediaSessionThumb + "-128.jpg",
+            sizes: "128x128",
+            type: "image/jpeg"
+          },
+          {
+            src: mediaSessionThumb + "-192.jpg",
+            sizes: "192x192",
+            type: "image/jpeg"
+          },
+          {
+            src: mediaSessionThumb + "-256.jpg",
+            sizes: "256x256",
+            type: "image/jpeg"
+          },
+          {
+            src: mediaSessionThumb + "-384.jpg",
+            sizes: "384x384",
+            type: "image/jpeg"
+          },
+          {
+            src: mediaSessionThumb + "-512.jpg",
+            sizes: "512x512",
+            type: "image/jpeg"
+          }
+        ]
+      });
+    }
+  },
+
   didInsertElement() {
     let url = `https://feeds.rasset.ie/livelistings/playlist/?source=rte.ie&platform=webradio&channelid=9&id=9&autoStart=false&thumbnail=&playertype=flash&header=auto&mainHeader=progTitle&subHeader=broadcastDate&highlightHeader=off&radioUI=true&callback=dummy`;
     if (this.clipId > 100) {
@@ -25,10 +73,12 @@ export default Component.extend({
       })
       .then(r => {
         if (this.clipId > 100) {
+          console.log(r.shows[0]);
           let audioUrl =
             r.shows[0]["media:group"][0].hls_server +
             r.shows[0]["media:group"][0].hls_url;
           this.set("audioUrl", audioUrl);
+          this.set("audioMeta", r.shows[0]);
         } else {
           this.set("audioUrl", r[0].fullUrl);
         }
@@ -54,6 +104,7 @@ export default Component.extend({
 
         this.myFlowplayer.player.on("ready", (e, api) => {
           console.log(api.video);
+          this.mediaSession();
           this.set("duration", api.video.duration);
         });
 
